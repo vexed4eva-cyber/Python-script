60b4d4d661eb2f172b3ee34e7c5ac474e5dcb95e
Exovex <vexed4eva@gmail.com>
2025-11-21 18:37:56 -0800

    Idk why
    Weeee2

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 type1 = input("mutliply,divde,Exponets addtion or subtraction (you can just use the first letter of it) ")
-num2 = float(input("Enter first number"))
-num3 = float(input("Enter second number"))
+num2 = float(input("Enter first number: "))
+num3 = float(input("Enter second number: "))
 if type1.lower in ["multiply","m","divide","d","exponets","e","add","addtion","a","subtraction","s"]:
 	valid_input = True
 if type1.lower in ["multiply","m"]:
 	print(num1 * num2)
 if type1.lower in ["divide","d","divshion"]:
 	print(num1 % num2)
+if type1.lower in ["e"]:
+	print(num1 ** num2)
